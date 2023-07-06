80e94765cf9a6f8012f2b9d7afbf858697e7add1
Andres Fernandez <andres94.af@gmail.con>
2023-07-06 17:04:39 +0200

    funcionalidad para agregar productos nuevos

--- src/app/component/productos/productos.component.ts
+++ src/app/component/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Producto } from 'src/app/models/models';
+import { Categoria, Producto } from 'src/app/models/models';
+import { CategoriaService } from 'src/app/service/categoria.service';
 import { ProductoService } from 'src/app/service/producto.service';
 
 @Component({
@@ -9,20 +10,35 @@ import { ProductoService } from 'src/app/service/producto.service';
   styleUrls: [],
 })
 export class ProductosComponent implements OnInit {
-
   titulo: string = 'Productos';
   productos: Producto[];
-  producto:Producto;
-  id:any;
-  valorBusqueda:any;
-  btnVisible:boolean = false;
-  cantidadProducto:number;
+  producto: Producto;
+  id: any;
+  valorBusqueda: any;
+  btnVisible: boolean = false;
+  cantidadProducto: number;
+  categorias: Categoria[];
+  datosFormulario = {
+    nombre: '',
+    informacion: '',
+    imgUrl: '',
+    precioUnitario: '',
+    idCategoria: '',
+  };
 
-  constructor(private route: ActivatedRoute, private productoService: ProductoService) {
-    this.route.paramMap.subscribe(params =>{
+  constructor(
+    private route: ActivatedRoute,
+    private productoService: ProductoService,
+    private categoriaService: CategoriaService
+  ) {
+    this.categoriaService.obtenerCategorias().subscribe((categoriasObt) => {
+      this.categorias = Object.values(categoriasObt);
+    });
+
+    this.route.paramMap.subscribe((params) => {
       this.id = params.get('id');
       this.valorBusqueda = params.get('valorBusqueda');
-    })
+    });
   }
 
   ngOnInit(): void {
@@ -31,8 +47,7 @@ export class ProductosComponent implements OnInit {
         .obtenerProductosPorCategoria(this.id)
         .subscribe((productosObt) => {
           this.productos = Object.values(productosObt);
-          if(this.productos.length != 0){
-            this.producto = this.productos[0];
+          if (this.productos.length != 0) {
             this.btnVisible = true;
           }
         });
@@ -41,26 +56,55 @@ export class ProductosComponent implements OnInit {
         .obtenerProductosFiltrados(this.valorBusqueda)
         .subscribe((productosObt) => {
           this.productos = Object.values(productosObt);
-          this.producto = this.productos[0];
           this.btnVisible = true;
         });
     } else {
       this.productoService.obtenerProductos().subscribe((productosObt) => {
         this.productos = Object.values(productosObt);
-        this.producto = this.productos[0];
       });
     }
-    
   }
 
-  verDetallesDelProducto(producto:Producto){
+  verDetallesDelProducto(producto: Producto) {
     this.producto = producto;
   }
 
-  //Por Hacer
-  agregarALista(producto:Producto){
-    console.log('Agregar: ' + producto.nombre + ' la cantidad de: ' + this.cantidadProducto);
+  verFormularioNuevoProducto() {
+    window.location.reload();
   }
 
+  guardarNuevoProducto() {
+    let producto: Producto = new Producto();
+    producto.nombre = this.datosFormulario.nombre;
+    producto.informacion = this.datosFormulario.informacion;
+    producto.imgUrl = this.datosFormulario.imgUrl;
+    producto.precioUnitario = Number(this.datosFormulario.precioUnitario);
+    let catId: string = this.datosFormulario.idCategoria;
 
+    if (
+      producto.nombre == '' ||
+      producto.informacion == '' ||
+      producto.imgUrl == '' ||
+      catId == '' ||
+      producto.precioUnitario == 0
+    ) {
+      return;
+    }
+    this.productoService
+      .generarNuevoProducto(catId, producto)
+      .subscribe((productoObt) => {
+        console.log(productoObt);
+        this.verFormularioNuevoProducto();
+      });
+  }
+
+  //Por Hacer
+  agregarAListaDeCompras(producto: Producto) {
+    console.log(
+      'Agregar: ' +
+        producto.nombre +
+        ' la cantidad de: ' +
+        this.cantidadProducto
+    );
+  }
 }
--- src/app/service/producto.service.ts
+++ src/app/service/producto.service.ts
@@ -1,24 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Producto } from '../models/models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductoService {
-  
   apiUrl: string = 'http://localhost:8080/producto';
-  
-  constructor(private http: HttpClient) { }
-  
+
+  constructor(private http: HttpClient) {}
+
   obtenerProductos() {
     return this.http.get(this.apiUrl);
   }
-  
-  obtenerProductosPorCategoria(id:number) {
-    return this.http.get(this.apiUrl + '/'+id);
+
+  obtenerProductosPorCategoria(id: number) {
+    return this.http.get(this.apiUrl + '/' + id);
   }
 
   obtenerProductosFiltrados(valorFiltro: string) {
     return this.http.get(this.apiUrl + '/filtrado/' + valorFiltro);
+  }
+
+  generarNuevoProducto(catId: string, producto: Producto) {
+    return this.http.post(this.apiUrl + '/' + catId, producto);
   }
 }
